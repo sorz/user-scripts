733e09690a23e4140fad47a869abcdddc7718608
sorz <me@sorz.org>
2018-04-15 23:28:40 +1000

    [hn-highlight] Restore threshold with sigmoid

--- ycombinator/hn-highlight.user.js
+++ ycombinator/hn-highlight.user.js
@@ -2,17 +2,19 @@
 // @name        Highlight high points post for HN
 // @namespace   org.sorz.lab.hn
 // @include     /^https://news.ycombinator.com/(news)?(\?p=\d+)?$/
-// @version     2
+// @version     3
 // @grant       none
 // ==/UserScript==
 
-const MAX_POINTS = 800;
+const THRESHOLD = 200;
+
+const sigmoid = t =>  1 / (1 + Math.exp(-t));
 
 document.querySelectorAll('.score').forEach(elem => {
   const points = elem.textContent.match(/\d+/)[0];
   const line = elem.closest('tr').previousElementSibling;
   const title = line.querySelector('td.title:last-child');
-  const score = Math.min(Math.round(points / MAX_POINTS * 100), 97);
+  const score = sigmoid((points - THRESHOLD) / 50) * 100;
   title.style = `
     background: linear-gradient(
       to right,
