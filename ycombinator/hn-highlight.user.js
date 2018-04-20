68625eabad49953855ef592ed8c0295e83b8232e
sorz <me@sorz.org>
2018-04-20 20:24:10 +1000

    [hn-highlight] set scale as variable & max limit

--- ycombinator/hn-highlight.user.js
+++ ycombinator/hn-highlight.user.js
@@ -2,11 +2,12 @@
 // @name        Highlight high points post for HN
 // @namespace   org.sorz.lab.hn
 // @include     /^https://news.ycombinator.com/(news)?(\?p=\d+)?$/
-// @version     3
+// @version     4
 // @grant       none
 // ==/UserScript==
 
 const THRESHOLD = 200;
+const SCALE = 50;
 
 const sigmoid = t =>  1 / (1 + Math.exp(-t));
 
@@ -14,13 +15,14 @@ document.querySelectorAll('.score').forEach(elem => {
   const points = elem.textContent.match(/\d+/)[0];
   const line = elem.closest('tr').previousElementSibling;
   const title = line.querySelector('td.title:last-child');
-  const score = sigmoid((points - THRESHOLD) / 50) * 100;
+  const score = sigmoid((points - THRESHOLD) / SCALE) * 100;
+  const percent = Math.min(score, 98);
   title.style = `
     background: linear-gradient(
       to right,
       #ddd 0%,
-      #ddd ${score}%,
-      rgba(0,0,0,0) ${score + 2}%,
+      #ddd ${percent}%,
+      rgba(0,0,0,0) ${percent + 2}%,
       rgba(0,0,0,0) 100%
     );`;
 });
